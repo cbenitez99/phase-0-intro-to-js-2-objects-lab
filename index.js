85f9681b004052d4cc5d00ab95b546d11dbc8217
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-05-26 15:45:19 -0500

    commit tree da1cf2659107c4b2fad5df948755af3241da0986

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-const employee = {
-    name: "Cristian",
-    streetAddress: "420 Fountain Lane"
-};
+// const employee = {
+//     name: "Cristian",
+//     streetAddress: "420 Fountain Lane"
+// };
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign({}, employee, {[key]: value});
-};
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign({}, employee, {[key]: value});
+// };
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign(employee, {[key]: value});
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign(employee, {[key]: value});
     
-};
+// };
 
-function deleteFromEmployeeByKey(employee, key) {
-    const newEmp = Object.assign({}, employee);
-    delete newEmp[key];
-    return newEmp;
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newEmp = Object.assign({}, employee);
+//     delete newEmp[key];
+//     return newEmp;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key];
+//     return employee;
+// }
+
+const employee = {
+    name: 'Cristian',
+    streetAddress: '321 XD Rd.'
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key]: value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign(employee, {[key]: value});
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = Object.assign({}, employee);
+    delete newObj[key];
+    return newObj;   
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key];
     return employee;
 }
